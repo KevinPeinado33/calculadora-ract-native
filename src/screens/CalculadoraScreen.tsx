b668cee6839fa2acb4cde1d28be9f5a361ff4dc5
kpeinado <kpeinado@everis.com>
2021-08-25 22:30:27 -0500

    fin aplicacion

--- src/screens/CalculadoraScreen.tsx
+++ src/screens/CalculadoraScreen.tsx
@@ -1,79 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { Text, View } from 'react-native';
 import { BotonCalc } from '../components/BotonCalc';
 import { styles } from '../theme/AppThem';
+import { useCalculadora } from '../hooks/useCalculadora';
 
 export const CalculadoraScreen = () => {
 
-    const [numeroAnterior, setNumeroAnterior] = useState('0');
-    const [numero, setNumero] = useState('0');
-
-    const limpiar = () => {
-        setNumero('0');
-    }
-
-    const armarNumero = ( numeroTexto: string ) => {
-
-        // no aceptar doble punto
-        if ( numero.includes('.') && numeroTexto === '.' ) return;
-
-        if ( numero.startsWith('0') || numero.startsWith('-0') ) {
-
-            // punto decimal
-            if( numeroTexto === '.' ) {
-                setNumero( numero + numeroTexto );
-                
-                // evaluar si hay otro numero, y hay un punto
-            } else if ( numeroTexto === '0' && numero.includes('.') ) {
-                setNumero( numero + numeroTexto );
-                
-                // evaular si es diferente de cero y no tiene un .
-            } else if ( numeroTexto !== '0' && !numero.includes('.') ) {
-                setNumero( numeroTexto );                
-                
-                // evitar 000.0
-            } else if ( numeroTexto === '0' && !numero.includes('.') ) {
-                setNumero( numero );                
-            } else {
-                setNumero( numero + numeroTexto );
-            }           
-
-        } else {
-            setNumero( numero + numeroTexto );
-        }
-        
-    }
-
-    const positivoNegativo = () => {
-        if ( numero.includes('-') ) {
-            setNumero( numero.replace('-', '') );
-        } else {
-            setNumero( '-' + numero );            
-        }
-    }
-
-    const btnDelete = () => {
-
-        let negativo = '';
-        let numeroTemp = numero;
-
-        if ( numero.includes('-') ) {
-            negativo = '-';
-            numeroTemp.substr(1);
-        }
-
-        if ( numeroTemp.length > 1 ) {
-            setNumero( negativo + numeroTemp.slice(0,-1))
-        } else {
-            setNumero('0');
-        }
-    }
-
+    const {
+        numero,
+        numeroAnterior,
+        limpiar,
+        positivoNegativo,
+        btnDelete,
+        btnDividir,
+        armarNumero,
+        btnMultiplicar,
+        btnRestar,
+        btnSumar,
+        calcular
+    } = useCalculadora();
+    
     return (
         <View style={ styles.calculadoraContainer}>
-
-            <Text style={styles.resultadoPequeno}>{ numeroAnterior }</Text>
+            {
+                ( numeroAnterior !== '0' ) && ( <Text style={styles.resultadoPequeno}>{ numeroAnterior }</Text> )
+            }
+            
             <Text 
                 style={styles.resultado}
                 numberOfLines={1}
@@ -86,7 +39,7 @@ export const CalculadoraScreen = () => {
                 <BotonCalc texto="C"   color="#9B9B9B" action={ limpiar } />
                 <BotonCalc texto="+/-" color="#9B9B9B" action={ positivoNegativo } />
                 <BotonCalc texto="del" color="#9B9B9B" action={ btnDelete } />
-                <BotonCalc texto="/"   color="#FF9427" action={ limpiar } />
+                <BotonCalc texto="/"   color="#FF9427" action={ btnDividir } />
             </View>
             
             {/* Fila de botones  */}
@@ -94,7 +47,7 @@ export const CalculadoraScreen = () => {
                 <BotonCalc texto="7" action={ armarNumero }  />
                 <BotonCalc texto="8" action={ armarNumero } />
                 <BotonCalc texto="9" action={ armarNumero } />
-                <BotonCalc texto="x" color="#FF9427" action={ limpiar } />
+                <BotonCalc texto="x" color="#FF9427" action={ btnMultiplicar } />
             </View>
 
             {/* Fila de botones  */}
@@ -102,7 +55,7 @@ export const CalculadoraScreen = () => {
                 <BotonCalc texto="4" action={ armarNumero } />
                 <BotonCalc texto="5" action={ armarNumero } />
                 <BotonCalc texto="6" action={ armarNumero } />
-                <BotonCalc texto="-" color="#FF9427" action={ limpiar } />
+                <BotonCalc texto="-" color="#FF9427" action={ btnRestar } />
             </View>
             
             {/* Fila de botones  */}
@@ -110,14 +63,14 @@ export const CalculadoraScreen = () => {
                 <BotonCalc texto="1"  action={ armarNumero } />
                 <BotonCalc texto="2"  action={ armarNumero } />
                 <BotonCalc texto="3"  action={ armarNumero } />
-                <BotonCalc texto="+"  color="#FF9427" action={ limpiar } />
+                <BotonCalc texto="+"  color="#FF9427" action={ btnSumar } />
             </View>
 
             {/* Fila de botones  */}
             <View style={styles.fila}> 
                 <BotonCalc texto="0" action={ armarNumero } ancho />
                 <BotonCalc texto="." action={ armarNumero } />
-                <BotonCalc texto="=" color="#FF9427" action={ limpiar } />
+                <BotonCalc texto="=" color="#FF9427" action={ calcular } />
             </View>
             
         </View>
